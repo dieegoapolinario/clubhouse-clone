e63a9faf4246c2c61a1d6642c907a1ba947ee899
Diego <dieego.apolinario@gmail.com>
2021-05-19 23:49:55 -0300

    update server/app
    Aula02 parte01

--- aula02/server/src/controllers/roomsController.js
+++ aula02/server/src/controllers/roomsController.js
@@ -14,6 +14,76 @@ export default class RoomsController {
     console.log('connection stablished with', id)
     this.#updateGlobalUserData(id)
   }
+
+  disconnect(socket) {
+    console.log('disconnect!!', socket.id)
+    this.#logoutUser(socket)
+  }
+
+  #logoutUser(socket) {
+    const userId = socket.id
+    const user = this.#users.get(userId)
+    const roomId = user.roomId
+    // remover user da lista de usuarios ativos
+    this.#users.delete(userId)
+
+    // caso seja um usuario sujeira que estava em uma sala que não existe mais
+    if(!this.rooms.has(roomId)) {
+      return;
+    }
+
+    const room = this.rooms.get(roomId)
+    const toBeRemoved = [...room.users].find(({ id }) => id === userId)
+        
+    // removemos o usuario da sala
+    room.users.delete(toBeRemoved)
+
+    // se não tiver mais nenhum usuario na sala, removemos a sala
+    if(!room.users.size) {
+      this.rooms.delete(roomId)
+      return;
+    }
+
+    const disconnectedUserWasAnOwner = userId === room.owner.id
+    const onlyOneUserLeft = room.users.size === 1 
+
+    // validar se tem somente um usuario ou se o usuario era o dono da sala
+    if(onlyOneUserLeft || disconnectedUserWasAnOwner) {
+      room.owner = this.#getNewRoomOwner(room, socket)
+    }
+
+    // atualiza a room no final
+    this.rooms.set(roomId, room)
+
+    // notifica a sala que o usuario se desconectou
+    socket.to(roomId).emit(constants.event.USER_DISCONNECTED, user)
+  }
+    
+  #notifyUserProfileUpgrade(socket, roomId, user) {
+    socket.to(roomId).emit(constants.event.UPGRADE_USER_PERMISSION, user)
+  }
+
+  #getNewRoomOwner(room, socket) {
+    const users = [...room.users.values()]
+    const activeSpeakers = users.find(user => user.isSpeaker)
+    // se quem desconectou era o dono, passa a liderança para o próximo
+    // se não houver speakers, ele pega o attendee mais antigo (primeira posição)
+    const [newOwner] = activeSpeakers ? [activeSpeakers] : users
+    newOwner.isSpeaker = true
+
+    const outdatedUser = this.#users.get(newOwner.id)
+    const updatedUser = new Attendee({
+      ...outdatedUser,
+      ...newOwner,
+    })
+
+    this.#users.set(newOwner.id, updatedUser)
+
+    this.#notifyUserProfileUpgrade(socket, room.id, newOwner)
+
+    return newOwner
+  }
+
   joinRoom(socket, { user, room }) {
     const userId = user.id = socket.id
     const roomId = room.id
@@ -28,12 +98,12 @@ export default class RoomsController {
     this.#notifyUsersOnRoom(socket, roomId, updatedUserData)
     this.#replyWithActiveUsers(socket, updatedRoom.users)
   }
-  
+
   #replyWithActiveUsers(socket, users) {
     const event = constants.event.LOBBY_UPDATED
     socket.emit(event, [...users.values()])
   }
-  
+
   #notifyUsersOnRoom(socket, roomId, user) {
     const event = constants.event.USER_CONNECTED
     socket.to(roomId).emit(event, user)
@@ -49,7 +119,9 @@ export default class RoomsController {
     })
 
     // definir quem é o dono da sala
-    const [owner, users] = existingRoom ? [currentRoom.owner, currentRoom.users] : [currentUser, new Set()]
+    const [owner, users] = existingRoom ?
+      [currentRoom.owner, currentRoom.users] :
+      [currentUser, new Set()]
 
     const updatedRoom = this.#mapRoom({
       ...currentRoom,
@@ -78,26 +150,29 @@ export default class RoomsController {
 
     return mappedRoom
   }
-    
+
   #updateGlobalUserData(userId, userData = {}, roomId = '') {
     const user = this.#users.get(userId) ?? {}
     const existingRoom = this.rooms.has(roomId)
 
     const updatedUserData = new Attendee({
-      ...user,
-      ...userData,
-      roomId,
-      // se for o unico na sala
-      isSpeaker: !existingRoom,
+        ...user,
+        ...userData,
+        roomId,
+        // se for o unico na sala
+        isSpeaker: !existingRoom,
     })
 
     this.#users.set(userId, updatedUserData)
 
     return this.#users.get(userId)
+
   }
 
   getEvents() {
-    const functions = Reflect.ownKeys(RoomsController.prototype).filter(fn => fn !== 'constructor').map(name => [name, this[name].bind(this)])
+    const functions = Reflect.ownKeys(RoomsController.prototype)
+      .filter(fn => fn !== 'constructor')
+      .map(name => [name, this[name].bind(this)])
 
     return new Map(functions)
   }
