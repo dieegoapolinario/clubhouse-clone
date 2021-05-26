cd2e152ddc1a15bc3ab6a399729eb2e6377ad07b
Diego <dieego.apolinario@gmail.com>
2021-05-26 14:24:37 -0300

    update login

--- aula05/app/pages/_shared/constants.js
+++ aula05/app/pages/_shared/constants.js
@@ -19,10 +19,23 @@ export const constants = {
   events: {
     USER_CONNECTED: 'userConnection',
     USER_DISCONNECTED: 'userDisconnection',
+    
     JOIN_ROOM: 'joinRoom',
     LOBBY_UPDATED: 'lobbyUpdated',
     UPGRADE_USER_PERMISSION: 'upgradeUserPermission',
+    
     SPEAK_REQUEST: 'speakRequest',
     SPEAK_ANSWER: 'speakAnswer'
-  }
+  },
+  
+  firebaseConfig: {
+    apiKey: "AIzaSyBlOf-lXRclE2LJf5avFWXKSF022kV2cy4",
+    authDomain: "clubhouse-clone-f6be7.firebaseapp.com",
+    projectId: "clubhouse-clone-f6be7",
+    storageBucket: "clubhouse-clone-f6be7.appspot.com",
+    messagingSenderId: "227157515158",
+    appId: "1:227157515158:web:5618e541a2608f2fe69bb7",
+    measurementId: "G-F5QDG7G09V"
+  },
+  storageKey: "jsexpert:storage:user"
 }
